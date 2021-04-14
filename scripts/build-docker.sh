4902ea17caa8e061b829e2093c8f6699ae8d9773
Krakaw <41575888+Krakaw@users.noreply.github.com>
2021-04-14 15:59:03 +0200

    Update build script

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
-TAG="${1:-latest}"
-NAME="${2:-proxy-auth}"
-DOCKER_BUILDKIT=1 docker build -t "$NAME:$TAG" --progress=plain .
+IMAGE_NAME="${IMAGE_NAME:-rauthy}"
+TAG="${TAG:-latest}"
+DOCKER_BUILDKIT=1 docker build -t "$IMAGE_NAME:$TAG" --progress=plain .
 
 if [ -n "$SSH_HOST" ]; then
   echo "Deploying to $SSH_HOST"
-  docker save "$NAME:$TAG" | bzip2 | pv | ssh -o 'RemoteCommand=none' "$SSH_HOST"  'bunzip2 | docker load'
+  docker save "$IMAGE_NAME:$TAG" | bzip2 | pv | ssh -o 'RemoteCommand=none' "$SSH_HOST"  'bunzip2 | docker load'
 else
   # shellcheck disable=SC2016
   echo 'Set $SSH_HOST to automatically deploy'
